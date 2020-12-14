be23bab28791eecf0f6d928d63e59e646b42d30e
supreeth1816 <supreet.venkata@gmail.com>
2020-12-15 02:52:19 +0530

    first

---
+++ src/post.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function post() {
+  return (
+    <div className="post">
+      <h5>supreeth1816</h5>
+      <img
+        className="post_image"
+        src="https://instagram.fhyd2-1.fna.fbcdn.net/v/t51.2885-19/s320x320/119191746_243680633648095_31082714764122343_n.jpg?_nc_ht=instagram.fhyd2-1.fna.fbcdn.net&_nc_ohc=BPrGFPT5DSwAX9jHLQ4&tp=1&oh=316d7fc9a2994fc4ab1425fee26b75e3&oe=5FFFDC74"
+        alt=""
+      />
+      <p className="user_caption">
+        <strong>supreeth1816</strong> Hello
+      </p>
+    </div>
+  );
+}
+
+export default post;
